a129de5cc15124484a4f456b55bea2bbd6b0c77b
TiagoJFil <tjgfil@gmail.com>
2021-11-18 11:37:48 +0000

    fixed filames with double .js

--- Borga-P1/borga-data-ext.js
+++ Borga-P1/borga-data-ext.js
@@ -3,19 +3,28 @@
 const fetch = require('node-fetch');
 
 const BOARD_ATLAS_BASE_URI = https://api.boardgameatlas.com/api/;
-
+/*
 const HTTP_SERVER_ERROR = 5;
 
 function getStatusClass(statusCode) {
 	return ~~(statusCode / 100); //como nao ha tipos em js , utilizamos o not bit a bit duas vezes para converter em inteiro
 }
+*/
 
 function do_fetch(uri) {
 	return fetch(uri)
-		.catch (err => { throw err; })
+		.catch (err => { throw errors.EXT_SVC_FAIL(err); })
 		.then(res => {
 			if (res.ok) {
 				return res.json();
+			} else {
+				if (getStatusClass(res.status) === HTTP_SERVER_ERROR) {
+					return res.json()
+						.catch (err => err) 
+						.then(info => { throw errors.EXT_SVC_FAIL(info); });
+				} else {
+					throw errors.FAIL(res); 
+				}
 			}
 		});
 }
