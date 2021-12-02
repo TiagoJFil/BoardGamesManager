6646d7f5566c5963dd178da12de33764b92f95bd
ZeMaheli <josemcv99@hotmail.com>
2021-12-02 16:33:43 +0000

    testes

--- BORGA-P1/Tests/borga-data-mem.test.js
+++ BORGA-P1/Tests/borga-data-mem.test.js
@@ -4,6 +4,8 @@ const test_data_int = require('../borga-data-mem.js');
 
 describe('CreateGroup function tests',() => {
     test('create group with existing user creates group', async () => {
-        test_data_int.createGroup('')
+        await test_data_int.createGroup('manel','xpto','xptos');
+        const a = await test_data_int.hasGroup('manel','xpto');
+        expect(a).toEqual(true);
     });
 });
