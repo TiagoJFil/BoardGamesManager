129edb4ea06fa24805b3ac60714422b5e5a77e18
TiagoJFil <tjgfil@gmail.com>
2021-12-08 17:16:31 +0000

    fixed a small mistype on the edigroup function on services and updated postman collections

--- Borga-P1/borga-web-api.js
+++ Borga-P1/borga-web-api.js
@@ -109,7 +109,7 @@ module.exports = function (services) {
 		try{
 		const groupOldName = req.body.name;
 		const groupNewName = req.body.newname;
-		const groupDesc = req.body.des;
+		const groupDesc = req.body.desc;
 		const newGroup = await services.editGroup(getBearerToken(req),groupOldName,groupNewName,groupDesc)
 		res.json(newGroup);
 
