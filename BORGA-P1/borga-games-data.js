ae7d08f4816cb0c52d22382b1f3023107ba744db
48347-AntonioCarvalho <a48347@alunos.isel.pt>
2021-12-08 09:30:10 +0000

    Added commentary and support for nodemon

--- BORGA-P1/borga-games-data.js
+++ BORGA-P1/borga-games-data.js
@@ -8,10 +8,20 @@ const BOARD_ATLAS_BASE_URI = 'https://api.boardgameatlas.com/api/search?';
 
 const HTTP_SERVER_ERROR = 5;
 
+/**
+ * convert status code to int
+ * @param statusCode 
+ * @returns {int}
+ */
 function getStatusClass(statusCode) {
 	return ~~(statusCode / 100); //como nao ha tipos em js , utilizamos o not bit a bit duas vezes para converter em inteiro
 }
 
+/**
+ * fetches uri's
+ * @param {string} uri 
+ * @returns {object} response or error
+ */
 function do_fetch(uri) {
 	return fetch(uri)
 		.catch (err => { throw errors.EXT_SVC_FAIL(err); })
@@ -30,6 +40,13 @@ function do_fetch(uri) {
 		});
 }
 
+
+
+/**
+ * transforms game object response to a more simplified object
+ * @param {object} gameInfo 
+ * @returns {object}
+ */
 function makeGameObj(gameInfo) {
 	return {
 		id: gameInfo.id,
@@ -45,6 +62,11 @@ function makeGameObj(gameInfo) {
 	
 }
 
+/**
+ * returns a game from the api searching by name
+ * @param {string} name 
+ * @returns 
+ */
 function getGameByName(name) {
 	const search_uri =BOARD_ATLAS_BASE_URI + '&name=' + name + '&client_id=' + CLIENT_ID;
 
@@ -58,6 +80,12 @@ function getGameByName(name) {
 		});
 }
 
+
+/**
+ * searches a game by its id in the api
+ * @param {string} id 
+ * @returns {object} game or error
+ */
 function getGameById(id) {
 	const search_uri =BOARD_ATLAS_BASE_URI + '&ids=' + id + '&client_id=' + CLIENT_ID;
 
@@ -71,19 +99,10 @@ function getGameById(id) {
 		});
 }
 
-
-function makeListObj(answer){
-	const gamesList = {}
-	let it = 0
-	const size = answer.length
-	while(it < size){
-		gamesList[it + 1] = makeGameObj(answer[it])
-		it++
-	}
-	return gamesList
-}
-
-
+/**
+ * transforms a rank query from the api in a object 
+ * @returns {object} with the top 10 games
+ */
 function getListPopularGames() { 
 	const search_uri =BOARD_ATLAS_BASE_URI + '&order_by=rank&limit=10&client_id=' + CLIENT_ID;
 	return do_fetch(search_uri)
@@ -95,6 +114,24 @@ function getListPopularGames() {
 			}
 		});
 }
+
+
+/**
+ * adds all games to an object
+ * @param {object} answer 
+ * @returns {object} gamesList
+ */
+ function makeListObj(answer){
+	const gamesList = {}
+	let it = 0
+	const size = answer.length
+	while(it < size){
+		gamesList[it + 1] = makeGameObj(answer[it])
+		it++
+	}
+	return gamesList
+}
+
 
 module.exports = {
 	getGameByName,
